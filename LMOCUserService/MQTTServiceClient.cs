a1cdd75ca12dac72db28f9c7b72bb68bb8280487
mariskadianti <mariskaserlydianti@gmail.com>
2018-12-17 19:45:02 +0700

    Add Login Service

--- LMOCUserService/MQTTServiceClient.cs
+++ LMOCUserService/MQTTServiceClient.cs
@@ -14,6 +14,7 @@ using System.Threading.Tasks;
 using uPLibrary.Networking.M2Mqtt;
 using uPLibrary.Networking.M2Mqtt.Messages;
 
+
 namespace LMOCUserService
 {
     public partial class MQTTServiceClient : ServiceBase
@@ -22,7 +23,15 @@ namespace LMOCUserService
         {
             public string UserId { get; set; }
             public string Status { get; set; }
+            public string MacAddress { get; set; }
         }
+
+        public class ServiceLoginStatus
+        {
+            public string user_id { get; set; }
+            public int status { get; set; }
+        }
+
         public class ResultMessage
         {
             public string result { get; set; }
@@ -39,8 +48,30 @@ namespace LMOCUserService
             OnStart(null);
         }
 
+        private System.Threading.Thread _thread;
+
         protected override void OnStart(string[] args)
         {
+            try
+            {
+
+                _thread = new System.Threading.Thread(DoWork);
+
+                // Start the thread.
+                _thread.Start();
+                
+            }
+            catch (Exception ex){
+                StreamWriter sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "serviceLogs.txt", true);
+                sw.WriteLine(DateTime.Now.ToString() + " " + ex.Message, EventLogEntryType.Error);
+                sw.Flush();
+                sw.Close();
+                
+            }
+        }
+
+        private void DoWork()
+        {            
             StreamWriter sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "serviceLogs.txt", true);
             sw.WriteLine(DateTime.Now.ToString() + " " + "ServiceStarted");
             sw.Flush();
@@ -48,8 +79,15 @@ namespace LMOCUserService
 
             string mqttServer = ConfigurationManager.AppSettings["MQTTServer"].ToString();
             MqttClient client = new MqttClient(mqttServer);
-            byte code = client.Connect(Guid.NewGuid().ToString(), "vsnbroker", "Password1!");
-            Console.WriteLine("Connecting To innodev.vnetcloud.com");
+            try
+            {
+                byte code = client.Connect(Guid.NewGuid().ToString(), "vsnbroker", "Password1!");
+            }
+            catch {
+                sw.WriteLine(DateTime.Now.ToString() + " " + "Broker Disconnect");
+                sw.Flush();
+            } 
+            Console.WriteLine("Connecting To " + mqttServer);
             if (client.IsConnected)
             {
                 Console.WriteLine("Client connection success");
@@ -60,15 +98,16 @@ namespace LMOCUserService
             }
 
             string topic = ConfigurationManager.AppSettings["TopicSubscribe"].ToString();
-            Console.WriteLine("Subscripe to webOnlineTrackingMalang");
+            sw.WriteLine(DateTime.Now.ToString() + " " + "subscribe " + topic);
+            sw.Flush();
             client.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
             ushort msgId = client.Subscribe(new string[] { topic },
                  new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
-            Console.WriteLine("Connect");
+            Console.WriteLine("Connect " + topic);
             //StreamWriter sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "serviceLogs.txt", true);
 
 
-            sw.WriteLine(DateTime.Now.ToString() + " " + "Connected To innodev.vnetcloud.com");
+            sw.WriteLine(DateTime.Now.ToString() + " " + "Connected To " + mqttServer);
             sw.Flush();
             sw.Close();
         }
@@ -79,16 +118,26 @@ namespace LMOCUserService
             Console.WriteLine("Received Message");
             Console.WriteLine("Topic: " + e.Topic);
             string getTopic = Encoding.UTF8.GetString(e.Message);
-            string UserId = getTopic.Split(',').First();
-            string Status = getTopic.Split(',').Last();
-            Console.WriteLine("UserId: " + UserId + " - " + Status);
+            string UserId = getTopic.Split(',')[0];
+            string Status = getTopic.Split(',')[1];
+            string MacAddress = Status == "online" ? getTopic.Split(',')[2] : Status == "offline"? " "  : getTopic.Split(',')[2];
+            Console.WriteLine("UserId: " + UserId + " - " + Status + " - " + MacAddress);
             StreamWriter sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "serviceLogs.txt", true);
-            sw.WriteLine(DateTime.Now.ToString() + " " + "UserId: " + UserId + " - " + Status);
+            sw.WriteLine(DateTime.Now.ToString() + " " + "UserId: " + UserId + " - " + Status + " - " + MacAddress);
             sw.Flush();
             sw.Close();
             try
             {
-                RunAsync(UserId, Status).Wait();
+                if (Status == "login")
+                {
+                    int stat = Convert.ToInt32(getTopic.Split(',')[2]);
+                    RunAsyncLogin(UserId, stat).Wait();
+                }
+                else
+                {
+                    RunAsync(UserId, Status, MacAddress).Wait();
+                }
+                
             }
             catch (Exception ex)
             {
@@ -119,18 +168,54 @@ namespace LMOCUserService
             result = await response.Content.ReadAsAsync<ResultMessage>();
             return result;
         }
-        static async Task RunAsync(string UserId, string Status)
+        static async Task RunAsync(string UserId, string Status, string MacAddress)
         {
-            // Update port # in the following line.
+            // Update port # in the following line.            
             UserStatus UserStatus = new UserStatus
             {
                 UserId = UserId,
-                Status = Status
+                Status = Status,
+                MacAddress = MacAddress
             };
 
-            Console.WriteLine("Updating status user...");
+            Console.WriteLine("Updating status user online...");
             await UpdateUserStatusAsync(UserStatus);
 
+        }
+
+        static async Task RunAsyncLogin(string UserId, int Status)
+        {
+            // Update port # in the following line.            
+            ServiceLoginStatus UserStatusLogin = new ServiceLoginStatus
+            {
+                user_id = UserId,
+                status = Status
+            };
+
+            Console.WriteLine("Updating status user login...");
+            await UpdateUserStatusLoginAsync(UserStatusLogin);
+
+        }
+        static async Task<ResultMessage> UpdateUserStatusLoginAsync(ServiceLoginStatus UserStatusLogin)
+        {
+            string apiurl = ConfigurationManager.AppSettings["APIURL"].ToString();
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(apiurl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            //Console.WriteLine("testtt");
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                $"api/Transaction?updateUserIsLoged", UserStatusLogin);
+            Console.WriteLine("User Status Updated Successfully.");
+            StreamWriter sw = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "serviceLogs.txt", true);
+            sw.WriteLine(DateTime.Now.ToString() + " Update Status Success");
+            sw.Flush();
+            sw.Close();
+            // Deserialize the updated product from the response body.
+            ResultMessage result = new ResultMessage();
+            result = await response.Content.ReadAsAsync<ResultMessage>();
+            return result;
         }
 
         protected override void OnStop()
